2b3aa7d767213411bc5e8738c43325fd5b3a09ab
santisan <125523558+sntiwey@users.noreply.github.com>
2023-02-22 17:26:52 -0600

    Add files via upload

---
+++ Tarea 3 gato 2.java
@@ -0,0 +1,16 @@
+public class gato{
+
+     public static void main (String[]  args){
+
+       //Gato que me gusto hacer porque se ve bonito 
+
+        System.out.println("   |  |    ");
+        System.out.println("   |  |    ");
+        System.out.println("___+__+___ ");
+        System.out.println("   |  |    ");
+        System.out.println("___+__+___ ");
+        System.out.println("   |  |    ");
+        System.out.println("   |  |    ");
+    }
+}
+
