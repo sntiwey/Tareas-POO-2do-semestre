ed0bbab92660c081da44c2ccdce6fb9950d55ea9
sntiwey <santiagosalas48@aragon.unam.mx>
2023-02-22 17:25:10 -0600

    Tarea 3.0

---
+++ Tarea 3 gato 1.java
@@ -0,0 +1,15 @@
+public class gato2{
+
+        public static void main(String[] args){
+
+        //Gato requerido por el profesor
+
+        System.out.println(" - - - - - ");
+        System.out.println(" |       | ");
+        System.out.println(" |       | ");
+        System.out.println(" |       | ");
+        System.out.println(" |       | ");
+        System.out.println(" - - - - - ");
+
+  }
+}
