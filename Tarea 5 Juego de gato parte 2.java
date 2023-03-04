68139720e493ba626ad724045bddbf399e9b457c
santisan <125523558+sntiwey@users.noreply.github.com>
2023-03-04 09:51:55 -0600

    Add files via upload

---
+++ Tarea 5 Juego de gato parte 2.java
@@ -0,0 +1,19 @@
+public class tarea4gato{
+
+        public static void main(String[] args){
+
+        String separado = "oxx xo_ __o";
+        String[] parte = separado.split(" ");
+        String p1 = parte[0];
+        String p2 = parte[1];
+        String p3 = parte[2];
+        System.out.println(" -----  ");
+        System.out.println(" |" + parte[0] +"|");
+        System.out.println(" |" + parte[1] +"|");
+        System.out.println(" |" + parte[2] +"|");
+        System.out.println(" -----  ");
+
+
+  }
+}
+
